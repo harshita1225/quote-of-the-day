09d8eed55cec7e60505d1396331b5043cd2168d6
harshita <harshita1225@gmail.com>
2022-09-26 15:49:40 +0200

    task completed

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const quotes = [
-  'Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.',
+  "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.",
   "Don't watch the clock; do what it does. Keep going.",
-  'The secret of getting ahead is getting started.',
-  'Well done is better than well said.',
-  'You miss 100% of the shots you don’t take.',
-  'A goal is a dream with a deadline.',
-  'Outstanding people have one thing in common: An absolute sense of mission.',
-  'Trying is winning in the moment.',
-  'Fall down seven times and stand up eight.',
+  "The secret of getting ahead is getting started.",
+  "Well done is better than well said.",
+  "You miss 100% of the shots you don’t take.",
+  "A goal is a dream with a deadline.",
+  "Outstanding people have one thing in common: An absolute sense of mission.",
+  "Trying is winning in the moment.",
+  "Fall down seven times and stand up eight.",
   "You just can't beat the person who never gives up.",
-  'There is little success where there is little laughter.',
-  'We cannot solve our problems with the same thinking we used when we created them.'
+  "There is little success where there is little laughter.",
+  "We cannot solve our problems with the same thinking we used when we created them.",
 ];
+
+function randomQuoteGenerator() {
+  const random = Math.trunc(Math.random() * 12) + 1;
+  //console.log(input);
+  return (input.value = quotes[random]);
+}
+
+const input = document.querySelector(".input");
+const button = document.querySelector("button");
+
+button.addEventListener("click", randomQuoteGenerator);
+window.addEventListener("load", randomQuoteGenerator);
